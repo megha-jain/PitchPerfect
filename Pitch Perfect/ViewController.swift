cb03fef0dbd0a5a60e1f25a383523da46fc51aad
Megha <to.megha.jain@gmail.com>
2015-02-25 23:46:25 -0800

    First commit of this app

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //To see documentation about a class, hold the option key & click
 
+    @IBOutlet weak var recordingInProgress: UILabel!
+    //Here we created a variable called recordingInProgress of type UILabel
+    //IB stands for interface builder. @IBOutlet tells us that this is not an ordinary variable, but is linked to the storyboard (or IB) somehow
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +27,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        //Hide the stop button
+        recordButton.enabled = true
+        stopButton.hidden = true
+    }
 
-
+    @IBAction func recordAudio(sender: UIButton) {
+        recordButton.enabled = false
+        stopButton.hidden = false
+        recordingInProgress.hidden = false
+        //TODO: Record the user's voice
+        println("in recordAudio")
+    }
+    @IBAction func stopRecording(sender: UIButton) {
+        recordingInProgress.hidden = true
+    }
 }
 
